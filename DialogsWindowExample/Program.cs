94c4325dc8b0593d114ae6ad1c27afd75081f5d0
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-22 15:04:15 +0700

    Создание отдельной точки входа. Балуемся с сервисами

---
+++ DialogsWindowExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DialogsWindowExample
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
