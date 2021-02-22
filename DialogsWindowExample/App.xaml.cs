f03dd78e09cd912470b5fa9bdeb6f508df27aa22
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-22 16:35:55 +0700

    Методы для создания хоста приложения

--- DialogsWindowExample/App.xaml.cs
+++ DialogsWindowExample/App.xaml.cs
@@ -1,8 +1,16 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace DialogsWindowExample
 {
     public partial class App : Application
     {
+
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
     }
 }
--- DialogsWindowExample/Program.cs
+++ DialogsWindowExample/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace DialogsWindowExample
 {
     public static class Program
     {
+        // Созданная точка входа приложения
         [STAThread]
         public static void Main()
         {
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Создаем хост
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            // Настраиваем хост
+            host_builder.UseContentRoot(Environment.CurrentDirectory); // Указываем рабочий каталог приложения
+            host_builder.ConfigureAppConfiguration((host, cfg) => 
+            {
+                // cfg - объект конфигурации, host - ссылка на хост
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true); // reloadOnChange - перечитываем, если файл изменился
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
